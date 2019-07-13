95be806167f424b759fbc9201b3816f23a0c25a5
carter_yu <carteryuwenzheng@163.com>
2019-07-13 18:41:28 +0800

    微信跳转详情页面

--- app/src/main/java/com/carter/javaAndroid/modules/wxarticle/ui/WxArticleListFragment.java
+++ app/src/main/java/com/carter/javaAndroid/modules/wxarticle/ui/WxArticleListFragment.java
@@ -9,8 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.carter.javaAndroid.R;
 import com.carter.javaAndroid.base.fragment.BaseFragment;
+import com.carter.javaAndroid.core.constant.ARouterPath;
 import com.carter.javaAndroid.core.constant.Constants;
 import com.carter.javaAndroid.modules.homepager.bean.ArticleItemBean;
 import com.carter.javaAndroid.modules.homepager.bean.ArticleListBean;
@@ -50,19 +52,36 @@ public class WxArticleListFragment extends BaseFragment<WxArticleListPresenter> 
     }
 
     private void initRecyclerView() {
-        mArticleList =  new ArrayList<>();
-        mAdapter = new WxArticleListAdapter(R.layout.adapter_wx_article_list_item,mArticleList);
-        mAdapter.setOnItemClickListener((adapter, view, position) -> {});
-        mAdapter.setOnItemChildClickListener((adapter, view, position) -> {});
+        mArticleList = new ArrayList<>();
+        mAdapter = new WxArticleListAdapter(R.layout.adapter_wx_article_list_item, mArticleList);
+        mAdapter.setOnItemClickListener((adapter, view, position) -> startArticleDetailPager(view, position));
+        mAdapter.setOnItemChildClickListener((adapter, view, position) -> {
+        });
 
         mRecyclerView.setLayoutManager(new LinearLayoutManager(_mActivity));
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setAdapter(mAdapter);
     }
 
+    private void startArticleDetailPager(View view, int position) {
+        if (mAdapter.getData().size() <= 0 || mAdapter.getData().size() < position) {
+            return;
+        }
+        ArticleItemBean itemBean = mAdapter.getData().get(position);
+        ARouter.getInstance().build(ARouterPath.ARTICLE_DETAIL_ACTIVITY)
+                .withInt(Constants.ARTICLE_ID, itemBean.getId())
+                .withString(Constants.ARTICLE_TITLE, itemBean.getTitle())
+                .withString(Constants.ARTICLE_LINK, itemBean.getLink())
+                .withBoolean(Constants.IS_COLLECTED, itemBean.isCollect())
+                .withBoolean(Constants.IS_SHOW_COLLECT_ICON, true)
+                .withInt(Constants.ARTICLE_ITEM_POSITION, position)
+                .withString(Constants.EVENT_BUS_TAG, Constants.MAIN_PAGER)
+                .navigation();
+    }
+
     private void initRefreshListener() {
         mRefreshLayout.setOnRefreshListener(refreshLayout -> {
-            mPresenter.refreshData(id,false);
+            mPresenter.refreshData(id, false);
             mRefreshLayout.finishRefresh();
         });
         mRefreshLayout.setOnLoadMoreListener(refreshLayout -> {
@@ -80,15 +99,15 @@ public class WxArticleListFragment extends BaseFragment<WxArticleListPresenter> 
     protected void initEventAndData() {
         assert getArguments() != null;
         id = getArguments().getInt(Constants.WX_CHAPTER_ID);
-        mPresenter.refreshData(id,true);
+        mPresenter.refreshData(id, true);
     }
 
     @Override
     public void showWxArticlelist(ArticleListBean articleListBean, boolean isRefresh) {
-        if (mAdapter == null){
+        if (mAdapter == null) {
             return;
         }
-        if (isRefresh){
+        if (isRefresh) {
             mArticleList = articleListBean.getDatas();
             mAdapter.replaceData(mArticleList);
         } else {
